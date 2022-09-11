dda3788ff191f911fd62fa68deeacf71f79446e5
Yuya Ohbayashi <yuya9445@gmail.com>
2022-09-11 23:33:53 +0900

    画像の無断転載を防ぐ方法アイデアを実装

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,66 +1,90 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
+// 右クリックの禁止
+
+window.addEventListener('DOMContentLoaded', function(){
+    var images = document.images
+    var cancelContextMenu = function(e){
+        e.preventDefault()
+    };
+    Array.prototype.forEach.call(images, function (elm) {
+        elm.addEventListener('contextmenu', cancelContextMenu)
+    })
+})
+
+// ドラッグの禁止
+
+window.addEventListener('DOMContentLoaded', function(){
+    var images = document.images
+    var cancelEvent = function(e){
+      e.preventDefault()
+    };
+    Array.prototype.forEach.call(images, function (elm) {
+      elm.addEventListener('contextmenu', cancelEvent)
+      elm.addEventListener('dragstart', cancelEvent)
+    })
+})
+
+// 画像のURLがわからないようにする
+
+window.addEventListener('DOMContentLoaded', function(){
+    var images = document.querySelectorAll('img[data-img-index]')
+    var imagePath = {
+        'img1': './logo192.png',
     }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+    var cancelEvent = function (e) {
+      e.preventDefault()
     }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
-  
+    Array.prototype.forEach.call(images, function (elm) {
+      const attr = elm.getAttribute('data-img-index')
+      elm.src = imagePath[attr]
+      elm.onload = function (e) {
+        const canvas = document.createElement('canvas')
+        const imageStyle = document.defaultView.getComputedStyle(elm, '')
+        const ctx = canvas.getContext('2d')
+        const imageWidth = imageStyle.width.replace(/px$/i, '')
+        const imageHeight = imageStyle.height.replace(/px$/i, '')
+        canvas.width = imageWidth
+        canvas.height = imageHeight
+        ctx.drawImage(elm, 0, 0, imageWidth, imageHeight)
+        ctx.scale(0.5, 0.5)
+        elm.parentElement.insertBefore(canvas, elm)
+        elm.parentElement.removeChild(elm)
+        canvas.addEventListener('contextmenu', cancelEvent)
+        canvas.addEventListener('dragstart', cancelEvent)
+      }
+    })
+  })
+
+// スクリーンショットの防止
+
+var addOverlay = function () {
+if (!document.body.classList.contains('ov')) {
+    document.body.classList.add('ov')
+}
+}
+var removeOverlay = function () {
+if (document.body.classList.contains('ov')) {
+    setTimeout(function () {
+    document.body.classList.remove('ov')
+    }, 300)
+}
+}
+var overlay = function () {
+    addOverlay()
+    removeOverlay()
+}
+var keyevent = function (e) {
+if (e.keyCode === 44) {
+    overlay()
+}
+if (e.metaKey && e.shiftKey){
+    addOverlay()
+} else if ( !e.metaKey || !e.shiftKey ) {
+    removeOverlay()
+}
+}
+window.addEventListener('keydown', keyevent)
+window.addEventListener('keyup', keyevent)
+window.addEventListener('blur', addOverlay)
+window.addEventListener('focus', removeOverlay)
